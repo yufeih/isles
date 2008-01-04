39f768710906f2268de4ce60d3402f8186e7f090
Nightin <none@none>
2008-01-04 08:14:01 +0000

    CodePlexTest First Version!!!
    git-svn-id: https://nine.svn.codeplex.com/svn@954 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5
    
    --HG--
    extra : convert_revision : e278fdac2dc251dd906de9a1d574785c7a6952ce

---
+++ CodePlexTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodePlexTest")]
+[assembly: AssemblyProduct("CodePlexTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1252cc68-5ef0-4434-82e7-f7681f8578ee")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
