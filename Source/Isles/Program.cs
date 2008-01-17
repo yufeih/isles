fd331c5b0caea35ab31ae7d5e78181d7ad76844f
Nightin <none@none>
2008-01-17 02:09:29 +0000

    git-svn-id: https://nine.svn.codeplex.com/svn@2158 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5
    --HG--
    extra : convert_revision : d9bf0a77a21c5c08c9d1614c5fe5ca69eef5b0be

--- Source/Isles/Program.cs
+++ Source/Isles/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Isles.Engine;
 
 namespace Isles
 {
@@ -9,10 +10,30 @@ namespace Isles
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+#if !DEBUG
+            try
+            {
+#endif
+
+            using (BaseGame game = new GameIsles())
             {
                 game.Run();
+
+                // Sucessfully exit the game
+                Log.NewLine();
+                Log.Write("Program terminated. Overall FPS: " + game.Profiler.OverallFPS);
             }
+#if !DEBUG
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message + "\n\nSee log file for error detailes.", "Isles");
+
+                Log.Write(e.Source + " - " + e.Message);
+                Log.NewLine();
+                Log.Write(e.StackTrace, false);
+            }   
+#endif
         }
     }
 }
