1c841504320628290442713ed8ee02148c428fae
Yufei Huang <yufeih@live.com>
2021-11-01 13:10:33 +0800

    Remove [STAThread]

--- src/Isles/Program.cs
+++ src/Isles/Program.cs
@@ -1,18 +1,5 @@
 // Copyright (c) Yufei Huang. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-
-namespace Isles
-{
-    public static class Program
-    {
-        [STAThread]
-        public static void Main()
-        {
-            using var game = new GameIsles();
-            game.Run();
-        }
-    }
-}
-
+using var game = new Isles.GameIsles();
+game.Run();
