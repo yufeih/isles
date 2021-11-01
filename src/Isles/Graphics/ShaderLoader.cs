13c77a2511277553d0ae18aa718276aba54a05e1
Yufei Huang <yufeih@live.com>
2021-11-01 13:29:54 +0800

    wip

--- src/Isles/Graphics/ShaderLoader.cs
+++ src/Isles/Graphics/ShaderLoader.cs
@@ -26,26 +26,8 @@ namespace Isles.Graphics
         {
             return _effectProcessor.Process(
                 new() { EffectCode = File.ReadAllText(path), Identity = new(path) },
-                new EffectContentProcessorContext())
+                null)
                 .GetEffectCode();
-        }
-
-        private class EffectContentProcessorContext : ContentProcessorContext
-        {
-            public override ContentBuildLogger Logger => null;
-            public override OpaqueDataDictionary Parameters { get; } = new();
-            public override TargetPlatform TargetPlatform => TargetPlatform.Windows;
-            public override GraphicsProfile TargetProfile => GraphicsProfile.HiDef;
-            public override string BuildConfiguration => "Release";
-            public override string OutputFilename => "";
-            public override string OutputDirectory => "";
-            public override string IntermediateDirectory => "";
-
-            public override void AddDependency(string filename) { }
-            public override void AddOutputFile(string filename) { }
-            public override TOutput BuildAndLoadAsset<TInput, TOutput>(ExternalReference<TInput> sourceAsset, string processorName, OpaqueDataDictionary processorParameters, string importerName) => default;
-            public override ExternalReference<TOutput> BuildAsset<TInput, TOutput>(ExternalReference<TInput> sourceAsset, string processorName, OpaqueDataDictionary processorParameters, string importerName, string assetName) => default;
-            public override TOutput Convert<TInput, TOutput>(TInput input, string processorName, OpaqueDataDictionary processorParameters) => default;
         }
     }
 }
