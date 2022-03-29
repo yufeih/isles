46eb4593b4cb171d58430c108ce02fa82438487a
Yufei Huang <yufeih@live.com>
2022-03-29 15:35:53 +0800

    Add prefab model

--- src/isles.core/state/Models.cs
+++ src/isles.core/state/Models.cs
@@ -35,3 +35,72 @@ public class LevelModel
         public Vector3 Position { get; init; }
     }
 }
+
+public class PrefabModel
+{
+    public string? Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public string? Model { get; init; }
+
+    public IReadOnlyDictionary<string, string>? Attachment { get; init; }
+
+    public int Icon { get; init; }
+
+    public int Snapshot { get; init; }
+
+    public string? HotKey { get; init; }
+
+    public string? Sound { get; init; }
+
+    public string? SoundCombat { get; init; }
+
+    public string? SoundDie { get; init; }
+
+    public float Alpha { get; init; }
+
+    public float ScaleBias { get; init; }
+
+    public float RotationZBias { get; init; }
+
+    public Vector2 ObstructorSize { get; init; }
+
+    public float ViewDistance { get; init; }
+
+    public Vector2 SpawnPoint { get; init; }
+
+    public bool IsUnique { get; init; }
+
+    public Vector2 Defense { get; init; }
+
+    public Vector2 AttackRange { get; init; }
+
+    public Vector2 Attack { get; init; }
+
+    public float AttackDuration { get; init; }
+
+    public float TrainingTime { get; init; }
+
+    public float Speed { get; init; }
+
+    public string[] Spells { get; init; } = Array.Empty<string>();
+
+    public int Lumber { get; init; }
+
+    public int Gold { get; init; }
+
+    public int Food { get; init; }
+
+    public int Health { get; init; }
+
+    public float Priority { get; init; }
+
+    public float ConstructionTime { get; init; }
+
+    public float CoolDown { get; init; }
+
+    public float AreaRadius { get; init; }
+
+    public string? Halo { get; init; }
+}
