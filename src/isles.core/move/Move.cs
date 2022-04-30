0ca91c8b10bf2e4ecf38958c24b8380d46863072
Yufei Huang <yufeih@live.com>
2022-04-30 16:08:54 +0800

    Adjust decceleration calculation

--- src/isles.core/move/Move.cs
+++ src/isles.core/move/Move.cs
@@ -50,13 +50,26 @@ public sealed class Move : IDisposable
 
     private readonly IntPtr _world = move_new();
 
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        move_delete(_world);
+    }
+
+    ~Move()
+    {
+        move_delete(_world);
+    }
+
     public unsafe void Update(float dt, Span<Movable> movables, PathGrid? grid = null)
     {
         var idt = 1 / dt;
 
         foreach (ref var m in movables)
         {
-            m._force = CalculateForce(idt, ref m);
+            var desiredVelocity = Vector2.Zero;
+            desiredVelocity += MoveToTarget(ref m);
+            m._force = CalculateForce(idt, ref m, desiredVelocity);
         }
 
         fixed (void* units = movables)
@@ -67,47 +80,50 @@ public sealed class Move : IDisposable
         foreach (ref var m in movables)
         {
             if (m._velocity.LengthSquared() > m.Speed * m.Speed * 0.001f)
-            {
                 UpdateRotation(dt, ref m);
-            }
         }
     }
 
-    private Vector2 CalculateForce(float idt, ref Movable m)
+    private Vector2 MoveToTarget(ref Movable m)
     {
-        var desiredDirection = Vector2.Zero;
-        var desiredVelocity = Vector2.Zero;
+        if (m.Target is null)
+            return default;
 
-        if (m.Target != null)
+        var toTarget = m.Target.Value - m.Position;
+
+        // Have we arrived at the target exactly?
+        var distanceSq = toTarget.LengthSquared();
+        if (distanceSq < m.Speed * m.Speed * 0.001f)
         {
-            var toTarget = m.Target.Value - m.Position;
-
-            // Have we arrived at the target exactly?
-            var distanceSq = toTarget.LengthSquared();
-            if (distanceSq < m.Speed * m.Speed * 0.001f)
-            {
-                m.Target = null;
-                return default;
-            }
-
-            // Should we start decelerating?
-            var decelerationDistance = 0.5f * m.Velocity.LengthSquared() / m.Acceleration;
-            if (distanceSq > decelerationDistance * decelerationDistance)
-            {
-                desiredDirection = Vector2.Normalize(toTarget);
-                desiredVelocity = desiredDirection * m.Speed;
-            }
+            m.Target = null;
+            return default;
         }
 
+        // Should we start decelerating?
+        var decelerationDistance = 0.5f * m.Velocity.LengthSquared() / m.Acceleration;
+        if (distanceSq > decelerationDistance * decelerationDistance)
+        {
+            return Vector2.Normalize(toTarget) * m.Speed;
+        }
+
+        return default;
+    }
+
+    private Vector2 CalculateForce(float idt, ref Movable m, Vector2 desiredVelocity)
+    {
         var force = (desiredVelocity - m.Velocity) * idt;
         var accelerationSq = force.LengthSquared();
 
         // Are we turning or following a straight line?
         var maxAcceleration = m.Acceleration == 0 ? m.Speed : m.Acceleration;
-        if (m.Decceleration != 0 && desiredDirection != default && m._velocity.LengthSquared() > m.Speed * m.Speed * 0.001f)
+        if (m.Decceleration != 0)
         {
-            var dot = Vector2.Dot(desiredDirection, Vector2.Normalize(m.Velocity));
-            maxAcceleration = MathHelper.Lerp(m.Decceleration, m.Acceleration, (dot + 1) / 2);
+            var v = desiredVelocity.Length() * m.Velocity.Length();
+            if (v != 0)
+            {
+                var lerp = (Vector2.Dot(desiredVelocity, m.Velocity) / v + 1) / 2;
+                maxAcceleration = MathHelper.Lerp(m.Decceleration, m.Acceleration, lerp);
+            }
         }
 
         // Cap max acceleration
@@ -137,17 +153,6 @@ public sealed class Move : IDisposable
             m._rotation += delta;
         else
             m._rotation -= delta;
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-        move_delete(_world);
-    }
-
-    ~Move()
-    {
-        move_delete(_world);
     }
 
     [DllImport(LibName)] public static extern IntPtr move_new();
